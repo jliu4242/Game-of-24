7695108100d321390b473f844c0a13e0eaa1696f
Jon Liu <your email>
2025-07-04 17:14:25 -0700

    feat: added input checking and evaluation

--- timedMode.js
+++ timedMode.js
@@ -1,40 +1,11 @@
-var numbers = [];
+
+let numbers = [];
 
 const suits = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "0", "J", "Q", "K"];
 const values = ["C", "D", "H", "S"];
 
-/** 
-window.onload = function() {
-    buildDeck();
-    shuffleDeck();
-}
 
-function buildDeck() {
-    let values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
-    let types = ["C", "D", "H", "S"];
-    deck = [];
-
-    for (let i = 0;i < types.length;i++) {
-        for (let j=0;j < values.length;j++) {
-            deck.push(values[j] + types[i]);
-        }
-    }
-
-    console.log(deck);
-}
-
-function shuffleDeck() {
-    for (let i = 0; i < deck.length;i++) {
-        let j = Math.floor(Math.random() * deck.length);
-        let temp = deck[i];
-        deck[i] = deck[j];
-        deck[j] = temp;
-    }
-     
-    console.log(deck);
-}
-*/
-
+// Function to generate random set of 4 cards each time
 function getCards() {
     const targetDiv = document.getElementById('given-cards');
     const images = targetDiv.querySelectorAll('img');
@@ -43,16 +14,62 @@ function getCards() {
         img.remove();
     });
 
+    numbers.length = 0;
+
     for (let i = 0;i < 4;i++) {
         const suit = suits[Math.floor(Math.random() * suits.length)];
         const value = values[Math.floor(Math.random() * values.length)];
 
         cardCode = suit + value;
 
-        numbers.push(Number(value))
+        switch (suit) {
+            case 'A':
+                numbers.push(1);
+                break;
+            case 'J':
+                numbers.push(11);
+                break;
+            case 'Q':
+                numbers.push(12);
+                break;
+            case 'K':
+                numbers.push(13);
+                break;
+            default:
+                numbers.push(Number(suit));
+        }
 
         let img = document.createElement("img");
         img.src = `https://deckofcardsapi.com/static/img/${cardCode}.png`
         document.getElementById('given-cards').appendChild(img);
+
+        console.log(numbers[i]);
     }
+}
+
+// Function to check the players guess
+// If guess is correct, adds points and calls getCards(), else produce error comment
+function checkGuess() {
+    let guess = document.getElementById('input').value;
+    const guessedNumbers = extractIntegers(guess);
+
+    if (guessedNumbers.sort().join(',') === numbers.sort().join(',')) {
+        const result = math.evaluate(guess);
+        if (result == 24) {
+            getCards();
+        } else {
+            // 
+        }
+    } else {
+        // Produce error comment
+    }
+
+    document.getElementById('input').value = '';
+}
+
+function extractIntegers(guess) {
+    const matches = guess.match(/\d+/g);
+    const integers = matches.map(Number);
+
+    return integers;
 }
